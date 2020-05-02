2beabb199604ca4a7d647c56e98d61abeac6bca0
ryandixon <dixon104@marhsall.edu>
2020-05-02 08:56:58 -0400

    fixed inventory screen

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,12 +4,4 @@ using UnityEngine;
 
 public class NPC : Character
 {
-    public void Interact(Character player = null)
-    {
-        if (GetComponent<BattleLaunchCharacter>() != null)
-        {
-            GetComponent<BattleLaunchCharacter>().PrepareBattle(player);
-
-        }
-    }   
 }
