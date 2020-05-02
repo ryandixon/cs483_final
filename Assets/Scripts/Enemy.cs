4e419a4001e81a68b3c0179afd7580c666e8b78b
ryandixon <dixon104@marhsall.edu>
2020-05-02 05:24:24 -0400

    fixed UI and defend button

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,40 +4,8 @@ using UnityEngine;
 
 public class Enemy : Character
 {
-    public void Act() {
-        int dieRoll = Random.Range(0, 2);
-        Character target = BattleController.Instance.GetRandomPlayer();
-        
-        switch(dieRoll) {
-            case 0:
-                Defend(2);
-                break;
-            case 1:
-                Spell spellToCast = GetRandomSpell();
-                if(spellToCast.spellType == Spell.SpellType.Heal) {
-                    target = BattleController.Instance.GetWeakestEnemy();
-                }
-                //Not enough mana so regular attack
-                if(!CastSpell(spellToCast, target)) {
-                    BattleController.Instance.DoAttack(this, target);
-                }
-                break;
-            case 2:
-                BattleController.Instance.DoAttack(this, target);
-                break;
-        }
-    }
 
     public void LootTable() {
         
     }
-
-    Spell GetRandomSpell() {
-        return spells[Random.Range(0, spells.Count - 1)];
-    }
-
-    //public override void Die() {
-        //base.Die();
-        //BattleController.Instance.characters[1].Remove(this);
-    //}
 }
--- Assets/Scripts/PartyMember.cs
+++ Assets/Scripts/PartyMember.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class PartyMember : Character
 {
-    //public override void Die() {
-        //base.Die();
-        //BattleController.Instance.characters[0].Remove(this);
-    //}
+
 }
